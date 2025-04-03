8b38ef0e07ff8aef06517bd9cb8286c7124e0530
jaishmino5 <105439213+jaishmino5@users.noreply.github.com>
2025-04-03 20:56:36 +0530

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load trained model
-model = joblib.load("Temperature_Prediction_Model")
+model = joblib.load("Temperature_Prediction_Model.pkl")
 
 # Streamlit UI
 st.title("Temperature Prediction App")
